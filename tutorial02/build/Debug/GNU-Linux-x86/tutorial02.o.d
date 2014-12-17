38eee9b540dafb695372e306682e3a6bb277817a
tpphu <vietean@gmail.com>
2014-12-18 00:56:40 +0700

    Add example by me

---
+++ tutorial02/build/Debug/GNU-Linux-x86/tutorial02.o.d
@@ -0,0 +1,32 @@
+build/Debug/GNU-Linux-x86/tutorial02.o: tutorial02.cpp \
+ ../Include/GL/glew.h ../Include/GL/freeglut.h \
+ ../Include/GL/freeglut_std.h ../Include/GL/freeglut_ext.h \
+ ../Include/ogldev_math_3d.h /usr/include/assimp/vector3.h \
+ /usr/include/assimp/./Compiler/pushpack1.h \
+ /usr/include/assimp/./Compiler/poppack1.h \
+ /usr/include/assimp/matrix3x3.h /usr/include/assimp/matrix4x4.h \
+ ../Include/ogldev_util.h ../Include/ogldev_types.h
+
+../Include/GL/glew.h:
+
+../Include/GL/freeglut.h:
+
+../Include/GL/freeglut_std.h:
+
+../Include/GL/freeglut_ext.h:
+
+../Include/ogldev_math_3d.h:
+
+/usr/include/assimp/vector3.h:
+
+/usr/include/assimp/./Compiler/pushpack1.h:
+
+/usr/include/assimp/./Compiler/poppack1.h:
+
+/usr/include/assimp/matrix3x3.h:
+
+/usr/include/assimp/matrix4x4.h:
+
+../Include/ogldev_util.h:
+
+../Include/ogldev_types.h:
